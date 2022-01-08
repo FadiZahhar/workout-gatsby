c7e65863bb285390f20e00a1b48857ebc96f1348
Fadi Zahhar <79freelancer@gmail.com>
2022-01-08 16:54:02 +0200

    cleaning the unused methods to continue the training

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <div>
+            
+            Footer output here
+        </div>
+    )
+}
+
+export default Footer
---
+++ src/pages/about.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Layout from '../components/Layout'
+import {page,text} from '../examples/about.module.css';
+import styled from 'styled-components';
+const about = () => {
+    return (
+        <Layout>
+                <h1>about page</h1>
+        </Layout>
+    )
+}
+
+export default about
