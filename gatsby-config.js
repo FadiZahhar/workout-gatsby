ec6b19f958c9357ed9c732aa0dd6a66a77514a0f
Fadi Zahhar <79freelancer@gmail.com>
2022-01-09 21:45:32 +0200

    adding the env config

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,9 @@
  * See: https://www.gatsbyjs.com/docs/gatsby-config/
  */
 
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
 module.exports = {
   /* Your site config here */
   siteMetadata:{
@@ -30,7 +33,7 @@ module.exports = {
     {
       resolve: `gatsby-source-contentful`,
       options: {
-        spaceId: `your_space_id`,
+        spaceId: `zmrwbz6pybl8`,
         // Learn about environment variables: https://gatsby.dev/env-vars
         accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
       },
