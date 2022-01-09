576ab8af76cac16c60c03eb9b09e69e4098770fa
Fadi Zahhar <79freelancer@gmail.com>
2022-01-09 12:02:31 +0200

    adding the fetch data js file for graphql

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Layout from '../components/Layout'
 import {StaticImage} from 'gatsby-plugin-image';
-
+import {Link} from 'gatsby';
 const about = () => {
     return (
               <Layout>
